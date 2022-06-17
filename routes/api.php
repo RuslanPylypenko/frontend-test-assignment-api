ce0dc881f1e0571f63b0d99d17e918d33a42c791
ruslanpylypenko <ruslan.pylypenko@socialboost.com.ua>
2022-06-17 23:26:35 +0300

    removed auth routes from api

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,8 @@
 
 use App\Http\Controllers\PositionsController;
 use App\Http\Controllers\UsersController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('positions', [PositionsController::class, 'index'])->name('positions.index');
 Route::get('users', [UsersController::class, 'index'])->name('users.index');
