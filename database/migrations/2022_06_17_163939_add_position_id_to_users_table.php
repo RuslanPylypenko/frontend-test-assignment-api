8d0f4c82f8733878d9f79df654c1d096e9b581b8
ruslanpylypenko <ruslan.pylypenko@socialboost.com.ua>
2022-06-17 19:49:49 +0300

    added migrations added models

--- database/migrations/2022_06_17_163939_add_position_id_to_users_table.php
+++ database/migrations/2022_06_17_163939_add_position_id_to_users_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('position_id');
+            $table->foreign('position_id')->references('id')->on('positions')->restrictOnDelete();
         });
     }
 
@@ -27,6 +26,9 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['position_id']);
+            $table->dropColumn('position_id');
+        });
     }
 };
